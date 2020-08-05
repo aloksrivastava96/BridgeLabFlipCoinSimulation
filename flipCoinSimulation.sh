2319847cfd885edd9993aeeffb282f3c7f6af518
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 15:34:14 +0530

    Functionality for If TIE then flip till the difference is 2

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -4,21 +4,30 @@ echo "This Problem displays the winner heads or tails"
 headcount=0
 tailcount=0
 
+function flipCoin() {
+toss=$((RANDOM%2))
+if ((toss==1))
+then
+	((headcount++))
+else
+	((tailcount++))
+fi
+}
+
 while (( ((headcount<21)) || ((tailcount<21)) ))
 do
-	toss=$((RANDOM%2))
-	if((toss==1))
-	then
-		((headcount++))
-	else
-		((tailcount++))
-	fi
+	flipCoin $headcount $tailcount
 done
 
 if ((headcount==tailcount))
 then
-	echo "It is a TIE"
-elif ((headcount>tailcount))
+	while (( (( $(($headcount-$tailcount))<2 )) || (( $(($tailcount-$headcount))<2 )) ))
+	do
+		flipCoin $headcount $tailcount
+	done
+fi
+
+if ((headcount>tailcount))
 then
 	echo "HEAD won by $(($headcount-$tailcount))"
 else
